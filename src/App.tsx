f756b6e47675f8da063f32d6374c9d1542d7c33e
jfcofer <juaffern@espol.edu.ec>
2025-07-11 15:13:41 -0500

    [FIX] Fixed HTML Meta

--- src/App.tsx
+++ src/App.tsx
@@ -6,30 +6,33 @@ import SelectorUI from './components/SelectorUI';
 function App() {
   return (
     <>
-      <Grid container spacing={5} justifyContent="center" alignItems="center">
+      <Grid container spacing={5} display="flex" justifyContent="center" alignItems="center">
 
         {/* Encabezado */}
-        <Grid size={{ xs: 12, md: 12 }}><HeaderUI /></Grid>
+        <Grid size={{ xs: 12, md: 12 }} sx={{
+          justifyContent: "center",
+          alignItems: "center",
+        }}><HeaderUI /></Grid>
 
         {/* Alertas */}
-        <Grid size={{ xs: 12, md: 12 }} container justifyContent="right" alignItems="center">
+        <Grid size={{ xs: 12, md: 12 }} display="flex" justifyContent="center" alignItems="center">
           <AlertUI description="No se preveen lluvias" />
         </Grid>
 
         {/* Selector */}
-        <Grid size={{ xs: 12, md: 3 }}><SelectorUI /></Grid>
+        <Grid size={{ xs: 12, md: 3 }} display="flex" justifyContent="center" alignItems="center"><SelectorUI /></Grid>
 
         {/* Indicadores */}
-        <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
+        <Grid size={{ xs: 12, md: 9 }} display="flex" justifyContent="center" alignItems="center">Elemento: Indicadores</Grid>
 
         {/* Gráfico */}
-        <Grid size={{ xs: 12, md: 6 }} sx={{ display: { xs: "none", md: "block" } }}>Elemento: Gráfico</Grid>
+        <Grid size={{ xs: 12, md: 6 }} sx={{ display: { xs: "none", md: "flex" } }} justifyContent="center" alignItems="center">Elemento: Gráfico</Grid>
 
         {/* Tabla */}
-        <Grid size={{ xs: 12, md: 6 }} sx={{ display: { xs: "none", md: "block" } }}>Elemento: Tabla</Grid>
+        <Grid size={{ xs: 12, md: 6 }} sx={{ display: { xs: "none", md: "flex" } }} justifyContent="center" alignItems="center">Elemento: Tabla</Grid>
 
         {/* Información adicional */}
-        <Grid size={{ xs: 12, md: 12 }}>Elemento: Información adicional</Grid>
+        <Grid size={{ xs: 12, md: 12 }} display="flex" justifyContent="center" alignItems="center">Elemento: Información adicional</Grid>
 
       </Grid >
     </>
--- src/components/HeaderUI.tsx
+++ src/components/HeaderUI.tsx
@@ -1,7 +1,7 @@
 import { Typography } from "@mui/material"
 export default function HeaderUI() {
   return (
-    <Typography variant="h2" component="h1" sx={{ fontWeight: 'bold' }} >
+    <Typography variant="h2" component="h1" align='center' sx={{ fontWeight: 'bold' }} >
       Dashboard
     </Typography>
   )
