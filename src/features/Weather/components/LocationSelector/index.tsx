24694691c060bb71eda2db04c4fe3cae8656f740
jfcofer <juaffern@espol.edu.ec>
2025-07-21 14:48:54 -0500

    [ADD] STyled Dashboard Header

--- src/features/Weather/components/LocationSelector/index.tsx
+++ src/features/Weather/components/LocationSelector/index.tsx
@@ -1,49 +1,44 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useWeatherContext } from './../../context/WeatherContext';
 
 interface Location {
   value: string
-  label: string
-  timezone: string
+  city: string
+  country: string
   lat: number
   lon: number
 }
 
 const locationsData: Location[] = [
-  { value: 'ec-quito', label: 'Quito, Ecuador', timezone: 'GMT-5', lat: -0.180653, lon: -78.467834 },
-  { value: 'ec-guayaquil', label: 'Guayaquil, Ecuador', timezone: 'GMT-5', lat: -2.170998, lon: -79.922359 },
-  // … añade más con sus coords reales
+  { value: 'ec-quito', city: 'Quito', country: 'Ecuador', lat: -0.180653, lon: -78.467834 },
+  { value: 'ec-guayaquil', city: 'Guayaquil', country: 'Ecuador', lat: -2.170998, lon: -79.922359 },
 ]
 export default function LocationSelector() {
   const { setLocation } = useWeatherContext()
   const [selectedValue, setSelectedValue] = useState<string>('ec-guayaquil')
 
-  const handleChange = (value: string) => {
-    setSelectedValue(value)
-    const loc = locationsData.find(loc => loc.value === value)
+  useEffect(() => {
+    const loc = locationsData.find((l) => l.value === selectedValue);
     if (loc) {
-      setLocation(loc.lat, loc.lon)
+      setLocation(loc.lat, loc.lon);
     }
-  }
+  }, [selectedValue, setLocation]);
 
   return (
-    <div className="flex flex-col gap-2 min-w-[280px]">
-      <label htmlFor="location" className="text-sm font-medium text-forest-700">
-      </label>
+    <div className="flex flex-col gap-2 w-1/3 px-10">
+      <label htmlFor="location" className="text-sm text-center font-medium text-green-900">
         Ubicación Geográfica
+      </label>
       <div className="relative">
         <select
           id="location"
           value={selectedValue}
-          onChange={e => handleChange(e.target.value)}
-          className="w-full bg-amber-400 border border-forest-200 rounded-md py-2 px-3 pr-8 text-sm text-forest-700 focus:outline-none focus:border-forest-500"
+          onChange={e => setSelectedValue(e.target.value)}
+          className="w-full bg-white border border-r-green-700 rounded-md py-2 px-3 pr-8 text-sm text-green-900  focus:border-green-900"
         >
-          <option value="" disabled>
-            Seleccionar ubicación
-          </option>
           {locationsData.map(loc => (
             <option key={loc.value} value={loc.value}>
-              {loc.label} — {loc.timezone}
+              {loc.city}
             </option>
           ))}
         </select>
